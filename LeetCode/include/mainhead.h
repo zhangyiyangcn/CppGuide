a8bf92ac02244942e89dcac9e23b80b0b064d0f7
zhangyiyang <3238405246@qq.com>
2025-05-24 20:41:53 +0800

    feat: fist commit

---
+++ LeetCode/include/mainhead.h
@@ -0,0 +1,17 @@
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <vector>
+#include <string>
+#include <stack>
+#include <iostream>
+
+
+#define ll long long
+
+
+
+
+
+
---
+++ LeetCode/src/multiqueue.cpp
@@ -0,0 +1,8 @@
+#include "mainhead.h"
+
+int main() {
+    std::unordered_set<int> uset;
+    uset.insert(1);
+    std::cout << uset.size() << std::endl;
+
+}
